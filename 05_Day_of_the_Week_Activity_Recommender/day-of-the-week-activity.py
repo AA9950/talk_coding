3509d26fc0bfdd5069cf6d34c77dd0eb9a738317
AA9950 <995081@pdsb.net>
2025-02-28 09:03:20 -0500

    added headers

--- 05_Day_of_the_Week_Activity_Recommender/day-of-the-week-activity.py
+++ 05_Day_of_the_Week_Activity_Recommender/day-of-the-week-activity.py
@@ -1,1 +1,24 @@
 # Ask the user to enter the current day of the week
+day = input("Enter the current day of the week: ")
+
+# Check what the day is and print the appropriate message
+if day == "Monday":
+    print("Start your week with a workout!")
+elif day == "Tuesday":
+    print("It's a great day to read a book!")
+elif day == "Wednesday":
+    print("Mid-week movie night!")
+elif day == "Thursday":
+    print("Try a new recipe!")
+elif day == "Friday":
+    print("Relax and enjoy the weekend!")
+elif day == "Saturday":
+    print("Go for a hike!")
+elif day == "Sunday":
+    print("Prepare for the week ahead with some self-care.")
+else:
+    print("Invalid day. Please check your input.")
+
+
+
+
